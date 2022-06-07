e0445fc8626c70d06b2a3db4b06a834b65f61ae2
Nigel Major <NigelWGMajor@users.noreply.github.com>
2022-06-07 09:21:57 -0500

    Pre-clipping mods

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import React from 'react'
-import { Navbar, Button, Alignment } from "@blueprintjs/core";
+import { Navbar, Button, Alignment, Icon } from "@blueprintjs/core";
 
 function App() {
+  const onSnipClick = async () => {
+    const { desktopCapturer, remote } = window.require("electron");
+    const screen = remote.screen;
+    try {
+      const sources = await desktopCapturer.getSources({ types: ["screen"] });
+      const entireScreenSource = sources.find(
+        (source) => source.name === "Entire Screen"
+          || source.name === 'Screen 1'
+        || source.Name === 'Screen 2'
+      );
+      if (entireScreenSource) {
+        console.log(entireScreenSource);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div className="App">
-        <Navbar className="bp4-dark">
-          <Navbar.Group align={Alignment.LEFT}>
-            <Navbar.Heading>Electron Snip</Navbar.Heading>
-            <Navbar.Divider />
-            <Button className="bp3-minimal" icon="settings" text="Settings" />
-            <Button className="bp3-minimal" icon="help" text="About" />
-          </Navbar.Group>
-        </Navbar>
-      <header className="App-header">
-        <noscript>You need to enable JavaScript to run this app.</noscript>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar className="bp4-dark">
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>Electron Snip</Navbar.Heading>
+          <Navbar.Divider />
+          <Button className="bp3-minimal" icon="settings" text="Settings" />
+          <Button className="bp3-minimal" icon="help" text="About" />
+          <Button
+            className="bp3-minimal"
+            icon="camera"
+            text="Snip"
+            onClick={onSnipClick}
+          />
+        </Navbar.Group>
+      </Navbar>
+      <main className="App-main">
+        <Icon icon="camera" iconSize={100} />
+        <p>Electron Snip</p>
+      </main>
     </div>
   );
 }
